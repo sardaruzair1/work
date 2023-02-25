15a7610c7d4626103664ba6256c3f5d22f3d3389
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-02-26 01:33:35 +0500

    calback

---
+++ calback.py
@@ -0,0 +1,16 @@
+class CallbackExample:
+    def __init__(self):
+        self.last_best_value = 0
+        self.last_best_running_time = 0
+
+    def my_callback(self, ls, cb_type):
+        stats = ls.statistics
+        obj = ls.model.objectives[0]
+        if obj.value > self.last_best_value:
+            self.last_best_running_time = stats.running_time
+            self.last_best_value = obj.value
+        if stats.running_time - self.last_best_running_time > 5:
+            print(">>>>>>> No improvement during 5 seconds: resolution is stopped")
+            ls.stop()
+        else:
+            print(">>>>>> Objective %d" % obj.value)
